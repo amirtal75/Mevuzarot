6ce3040fabfd578e1510b608c145c572a4067acd
amirtal75 <46451642+amirtal75@users.noreply.github.com>
2019-12-22 13:04:44 +0200

    Update Manager.java

--- Project1/src/main/java/Manager.java
+++ Project1/src/main/java/Manager.java
@@ -72,7 +72,7 @@ public class Manager {
 
         while (!shouldTerminate) {
             System.out.println(" numberOfTasks: " + numberOfTasks.get());
-            if (numberOfTasks.get() % 150 == 0) {
+            if (numberOfTasks.get() % 80 == 0) {
                 Instance instance = ec2.createInstance(1,1,workerUserData).get(0);
                 ec2.attachTags(instance,"worker");
                 System.out.println("created new worker instance: " + instance.getInstanceId());
