1ea4a1162be523da1f3f97cc640f304fb9977f95
amirtal75 <46451642+amirtal75@users.noreply.github.com>
2019-12-22 17:30:05 +0200

    Update InputThread.java

--- Project1/src/main/java/InputThread.java
+++ Project1/src/main/java/InputThread.java
@@ -69,7 +69,7 @@ public class InputThread implements Runnable {
 
 
                 while ((currLine = inputFileFromLocalApp.readLine()) != null) {
-
+                    System.out.println("inside input thread, numberOfTasks: " + numberOfTasks.get() + "\nnumber wof instances: " + ec2. getInstances("").size());
                     if (numberOfTasks.get() % 80 == 0 && ec2. getInstances("").size()-1 <= numberOfTasks.get() / 80) {
                         Instance instance = ec2.createInstance(1,1,workerUserData).get(0);
                         ec2.attachTags(instance,"worker");
