8f6b27df06f48dc73314bf0c770bbd19924e8270
amirtal75 <46451642+amirtal75@users.noreply.github.com>
2019-12-22 14:16:53 +0200

    Update OutputThread.java

--- Project1/src/main/java/OutputThread.java
+++ Project1/src/main/java/OutputThread.java
@@ -10,28 +10,28 @@ public class OutputThread implements Runnable {
 
     String summeryFilesIndicatorQueue;
     Queue queue;
-    String QueueUrlLocalApps;
     ConcurrentHashMap<Integer, InputFileObject> InputFileObjectById;
     List<Message> currMessageQueue = new ArrayList<Message>();
     S3Bucket s3;
     String myQueueUrl2; //queue for outputJobs , should be passed to workers as well
     AmazonEC2 ec2;
+    ConcurrentHashMap<Integer, StringBuilder> stringResultsById; // will be passed by manager(the refference) by constructor
     boolean toTerminate;
 
-    public OutputThread(String myQueueUrl2, ConcurrentHashMap<Integer, InputFileObject> inputFileObjectById, String QueueUrlLocalApps, String summeryFilesIndicatorQueue) throws Exception {
+    public
+    OutputThread(String myQueueUrl2, ConcurrentHashMap<Integer, InputFileObject> inputFileObjectById, ConcurrentHashMap<Integer, StringBuilder> stringResultsById, String summeryFilesIndicatorQueue) throws Exception {
         this.queue = new Queue();
         this.myQueueUrl2 = myQueueUrl2;
-        this.QueueUrlLocalApps = QueueUrlLocalApps;
+        this.summeryFilesIndicatorQueue = summeryFilesIndicatorQueue;
         this.s3 = new S3Bucket();
         this.InputFileObjectById = inputFileObjectById;
-        this.summeryFilesIndicatorQueue = summeryFilesIndicatorQueue;
+        this.stringResultsById = stringResultsById;
         toTerminate = false;
     }
-    int i =1;
-    public void run() {
-        BufferedWriter writer = null;
-        ArrayList<String> completedreviewIDlist= new ArrayList<>();
-        String path = "/home/ubuntu/Mevuzarot-master/Project1/src/main/java/";
+
+    public
+    void run() {
+        ArrayList<String> completedreviewIDlist = new ArrayList<>();
         System.out.println("In Output Thread: " + Thread.currentThread());
         while (!toTerminate) {
             try {
@@ -39,43 +39,37 @@ public class OutputThread implements Runnable {
             } catch (Exception e) {
                 e.printStackTrace();
             }
-            if (!currMessageQueue.isEmpty()){
-                Message currMessege = currMessageQueue.get(0);
-                System.out.println("Received message number " + i + " content: " + currMessege.getBody());
-                ++i;
-                String[] resultContent = currMessege.getBody().split("@");
-                int inputFileId = Integer.parseInt(resultContent[0]);
-                InputFileObject currInputFileObj = InputFileObjectById.get(inputFileId);
-                //String result = inputFileId + "@" + reviewId + "@" + currIndicator + "@" + reviewText + "@" + reviewEntities +"@"+ sentiment;
 
-                writer = currInputFileObj.getWriter();
+            Message currMessege = currMessageQueue.get(0);
+            System.out.println("Received message content: " + currMessege.getBody());
+            String[] resultContent = currMessege.getBody().split("@");
+            int inputFileId = Integer.parseInt(resultContent[0]);
+            //String result = inputFileId + "@" + reviewId + "@" + currIndicator + "@" + reviewText + "@" + reviewEntities +"@"+ sentiment;
+            if (stringResultsById.containsKey(inputFileId))
+                stringResultsById.get(inputFileId).append(currMessege.getBody() + "\n"); //append all the reviews for one inputFile and seperate by "\n"
+                //check again what I sent to the local app
+            else {
+                stringResultsById.put(inputFileId, new StringBuilder()); // if is absent
+            }
+
+            InputFileObject currInputFileObj = InputFileObjectById.get(inputFileId);
+            currInputFileObj.increaseOutputLines();
+            currInputFileObj.CheckAndSetAllWorkersDone();
+            System.out.println("All workers done: " + currInputFileObj.getAllWorkersDone().get());
+            String inputFilename = currInputFileObj.getInputFilename();
+
+            if (currInputFileObj.getAllWorkersDone().get()) {// if all workers done
+                FileOutputStream outputFile = null;
                 if (!completedreviewIDlist.contains(resultContent[1])) {
-                    try {
-                        writer.write(currMessege.getBody() + "\n");
-                        System.out.println("increasing output from: " + currInputFileObj.getOutputLines());
-                        currInputFileObj.increaseOutputLines();
-                        System.out.println("after increase number og output lines: " + currInputFileObj.getOutputLines());
 
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    //stringResultsById.get(inputFileId).append(currMessege.getBody() + "\n"); //append all the reviews for one inputFile and seperate by "\n"
-                    completedreviewIDlist.add(resultContent[1]);
-                }
-                
-                    //check again what I sent to the local app
-                currInputFileObj.CheckAndSetAllWorkersDone();
-                System.out.println("All workers done: " + currInputFileObj.getAllWorkersDone().get());
-                String inputFilename = currInputFileObj.getInputFilename();
-
-                if (currInputFileObj.getAllWorkersDone().get()) {// if all workers done
                     try {
                         String outputName = inputFilename + "$";
                         //added "$" to the name because I dont want exact names for the input file and output file
-                        writer.flush();
+                        Writer writer = new BufferedWriter(new FileWriter(outputName)); //write to the output file
+                        writer.write(stringResultsById.get(inputFileId).toString());
 
-                        s3.upload(path, outputName);
-                        System.out.println("sending finished output file to local app" + summeryFilesIndicatorQueue);
+                        s3.upload("", outputName);
+                        System.out.println("Upload finised test: " + s3.downloadObject(outputName).getObjectContent().toString());
                         queue.sendMessage(summeryFilesIndicatorQueue, outputName); // outputFilename = key ??????
                     } catch (IOException e) {
                         e.printStackTrace();
@@ -85,17 +79,11 @@ public class OutputThread implements Runnable {
                 }
                 try {
                     queue.deleteMessage(myQueueUrl2, currMessege);
-                }
-                catch (Exception e) {
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
-            try {
-                Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
         }
     }
 }
+
