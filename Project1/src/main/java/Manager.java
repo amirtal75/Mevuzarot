b0cae7118c27909d380d5c20fb6d36ec2c1b96b3
amirtal75 <46451642+amirtal75@users.noreply.github.com>
2019-12-25 00:24:39 +0200

    Add files via upload

--- Project1/src/main/java/Manager.java
+++ Project1/src/main/java/Manager.java
@@ -56,11 +56,11 @@ public class Manager{
         // Create Thread Pools
         boolean run = true;
         while (continueRunning.get()) {
-            if (numberOfReceivedtasksFromTotalOfLocals.get() == numberOfCompletedTasks.get()) {
+            //if (numberOfReceivedtasksFromTotalOfLocals.get() == numberOfCompletedTasks.get()) {
                 System.out.println("Manager numberOfReceivedtasksFromTotalOfLocals is :" + numberOfReceivedtasksFromTotalOfLocals.get());
                 System.out.println("Manager number Of Tasks sent to workers are: " + numberOfTasks.get());
                 System.out.println("Manager number Of Tasks received from workers (built into a buffer): " + numberOfCompletedTasks.get());
-            }
+           // }
 
 
 
@@ -125,6 +125,7 @@ public class Manager{
                 }
 
             }
+            Thread.sleep(2000);
 
         }
     }
