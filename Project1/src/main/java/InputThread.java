1848400b9b0cb3b241e4fe3773a133e9aa8b3806
amirtal75 <46451642+amirtal75@users.noreply.github.com>
2019-12-26 10:06:55 +0200

    Update InputThread.java

--- Project1/src/main/java/InputThread.java
+++ Project1/src/main/java/InputThread.java
@@ -30,7 +30,7 @@ public class InputThread implements Runnable {
         boolean readAllLines = false;
         while (!readAllLines) {
 
-            createworker(ec2, numberOfTasks.get());
+
             synchronized (currFileObject) {
                 System.out.println("\n" + originator + " is increasing input lines of: " + currFileObject.getInputFilename() + "from: " + currFileObject.getInputLines());
                 job = currFileObject.readLine();
@@ -42,31 +42,4 @@ public class InputThread implements Runnable {
         }
         System.out.println("InputThread: " + Thread.currentThread() + " finished running");
     }
-
-    public void createworker(EC2Object ec2, int numberOfTasks){
-
-        int workerinstances = ec2.getInstances("").size() - 1;
-        Boolean tasksDivides = (numberOfTasks % 80) == 0;
-        int tasks = numberOfTasks/80;
-        Boolean condition = tasksDivides == false && workerinstances <= (tasks);
-
-        if ( condition == false || workerinstances > 15){
-            return;
-        }
-
-        // create user data dor workers
-        String getProject = "wget https://github.com/amirtal75/Mevuzarot/archive/master.zip\n";
-        String unzip = getProject + "sudo unzip -o master.zip\n";
-        String goToProjectDirectory = unzip + "cd Mevuzarot-master/Project1/\n";
-        String removeSuperPom = goToProjectDirectory + "sudo rm pom.xml\n";
-        String setWorkerPom = removeSuperPom + "sudo cp workerpom.xml pom.xml\n";
-        String buildProject = setWorkerPom + "sudo mvn -T 4 install -o\n";
-        String createAndRunProject = "sudo java -jar target/Project1-1.0-SNAPSHOT.jar\n";
-        String workerUserData = "#!/bin/bash\n" + "cd home/ubuntu/\n" + buildProject + createAndRunProject;
-
-        Instance instance = ec2.createInstance(1, 1, workerUserData).get(0);
-        ec2.attachTags(instance, "worker");
-        System.out.println("created new worker instance: " + instance.getInstanceId() + "\n\n\n\n");
-    }
-
 }
