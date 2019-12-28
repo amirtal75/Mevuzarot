2aa225ef31b11c10769fab2de61de82c4be69e64
amirtal75 <46451642+amirtal75@users.noreply.github.com>
2019-12-28 06:41:31 +0200

    Update Worker.java

--- Project1/src/main/java/Worker.java
+++ Project1/src/main/java/Worker.java
@@ -7,6 +7,7 @@ import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
 import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.util.CoreMap;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -45,7 +46,7 @@ public class Worker {
                     //System.out.println("number of result ; "+ i + "the result is " + result);
 
                     System.out.println("sending the result of worker to the completed queue: " + reviewAttributes[0]);
-                    queue.sendMessage(reviewAttributes[0], result);
+                    queue.sendMessage("completedJobQueue-" + reviewAttributes[0], result,"Worker:");
                     ////System.out.println("message was sent, deleting the task");
                     queue.deleteMessage(workerJobQueue, currJob); // we need to check befor deleting if we succeed to send the message
                 } else{
