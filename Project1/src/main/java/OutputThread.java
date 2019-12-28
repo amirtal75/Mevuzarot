7a1c3208969d95fe9042135345d2f4fecaef65af
amirtal75 <46451642+amirtal75@users.noreply.github.com>
2019-12-28 09:01:14 +0200

    Update OutputThread.java

--- Project1/src/main/java/OutputThread.java
+++ Project1/src/main/java/OutputThread.java
@@ -35,12 +35,12 @@ public class OutputThread implements Runnable {
 
                 Message currMessege = messagefromCompletedTasksQueue.get(0);
                 String[] resultContent = currMessege.getBody().split(delimiter);
-                // String result = inputFileId + delimiter + reviewId + delimiter + currIndicator + delimiter + reviewText + delimiter + reviewEntities +delimiter+ sentiment;
+                String result = inputFileId + delimiter + reviewId + delimiter + currIndicator + delimiter + reviewText + delimiter + reviewEntities +delimiter+ sentiment;
                 synchronized (currFileObject) {
-                    //System.out.println("\n" + originator + " is increasing output lines of: " + currFileObject.getInputFilename() + " from: " + currFileObject.getOutputLines());
+                    System.out.println("\n" + originator + " is increasing output lines of: " + currFileObject.getInputFilename() + " from: " + currFileObject.getOutputLines());
                     currFileObject.appendToBuffer(currMessege.getBody(), resultContent[1],originator);
                     wroteAllLines = currFileObject.getAllWorkersDone();
-                    //System.out.println("to: " + currFileObject.getOutputLines() + " and all lines read status = " + wroteAllLines + "\n");
+                    System.out.println("to: " + currFileObject.getOutputLines() + " and all lines read status = " + wroteAllLines + "\n");
                 }
                 queue.deleteMessage(completedTasksQueue, currMessege);
                 numberOfCompletedTasks.incrementAndGet();
