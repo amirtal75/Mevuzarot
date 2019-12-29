5fc9a501726cf6119cc4a9c3c86c31cdc571e818
amirtal75 <46451642+amirtal75@users.noreply.github.com>
2019-12-29 03:29:17 +0200

    Update Manager.java

--- Project1/src/main/java/Manager.java
+++ Project1/src/main/java/Manager.java
@@ -144,9 +144,8 @@ public class Manager{
         // asuming we ha a large number of message sent simultaniousley, we will wait for 1 minute before completeing the termiantion
         Thread.sleep(60000);
         System.out.println("\n Manager termiante deleting resources\n");
-        for (String url : queue.getQueueList()) {
-            queue.deleteQueue(url);
-        }
+        queue.deleteQueue("QueueUrlLocalApps", "");
+        queue.deleteQueue("workerJobQueue", "");
         ec2.terminateInstances(null);
     }
 
