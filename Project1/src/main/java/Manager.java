ce1f992c584872ff5da0033dcedc9368dfb755ad
amirtal75 <46451642+amirtal75@users.noreply.github.com>
2019-12-29 02:50:47 +0200

    Update Manager.java

--- Project1/src/main/java/Manager.java
+++ Project1/src/main/java/Manager.java
@@ -31,12 +31,10 @@ public class Manager{
         String path = "/home/ubuntu/Mevuzarot-master/Project1/src/main/java/";
         ConcurrentHashMap<String, InputFileObject> InputFileObjectById = new ConcurrentHashMap<>();
 
-        System.out.println("Thread pools creation");
         // Create Thread Pools
         ExecutorService poolForInput = Executors.newCachedThreadPool();
         ExecutorService poolForOutput = Executors.newCachedThreadPool();
 
-        System.out.println("\nManager Run:");
         // Loop Split variables
         String inputFileName;
         int numberOfLinesInTheLocalAppFile = 0;
@@ -50,25 +48,14 @@ public class Manager{
         int allWorkersNeeded = 0;
         int numberOfActiveWorkers = 0;
         while (continueRunning.get()) {
-            /*if (numberOfReceivedtasksFromTotalOfLocals.get() == numberOfCompletedTasks.get()) {
-            System.out.println("Manager numberOfReceivedtasksFromTotalOfLocals is :" + numberOfReceivedtasksFromTotalOfLocals.get());
-            System.out.println("Manager number Of Tasks sent to workers are: " + numberOfTasks.get());
-            System.out.println("Manager number Of Tasks received from workers (built into a buffer): " + numberOfCompletedTasks.get());
-            }*/
 
-            System.out.println("Before first worker start:");
             numberOfWorkersNeededForFile = numberOfLinesInTheLocalAppFile / 100;
-            System.out.println("numberOfWorkersNeededForFile: " + numberOfWorkersNeededForFile);
             workersForRest = (numberOfReceivedtasksFromTotalOfLocals.get() - numberOfCompletedTasks.get())/100;
-            System.out.println("numberOfReceivedtasksFromTotalOfLocals: " + numberOfReceivedtasksFromTotalOfLocals.get());
-            System.out.println("numberOfCompletedTasks: " + numberOfCompletedTasks.get());
             allWorkersNeeded = numberOfWorkersNeededForFile+workersForRest;
-            System.out.println("allWorkersNeeded: " + allWorkersNeeded);
-            numberOfActiveWorkers = ec2.getInstances("worker").size();
+            numberOfActiveWorkers = ec2.getInstances("").size() - ec2.getInstances("manager").size();
             for (int i = 0; i< allWorkersNeeded-numberOfActiveWorkers; i++){
                 createworker(ec2);
             }
-            System.out.println("Before receive message:");
 
             // Recieve message from local app queue
             currMessageQueue = queue.recieveMessage(QueueUrlLocalApps, 1, 1000); // check about visibility
@@ -83,10 +70,10 @@ public class Manager{
                 terminationIndicator = messageContent[3];
 
                 // Print process input message split results
-                System.out.println();
+                /*System.out.println();
                 System.out.println("Input From Local App processing split results:");
                 System.out.println("inputFileName: " + inputFileName + "numberOfLinesInTheLocalAppFile: " + numberOfLinesInTheLocalAppFile);
-                System.out.println("summeryFilesIndicatorQueue: " + summeryFilesIndicatorQueue + "terminationIndicator: " + terminationIndicator);
+                System.out.println("summeryFilesIndicatorQueue: " + summeryFilesIndicatorQueue + "terminationIndicator: " + terminationIndicator);*/
 
                 //Print input message process results
                 numberOfReceivedtasksFromTotalOfLocals = new AtomicInteger(numberOfReceivedtasksFromTotalOfLocals.get() + numberOfLinesInTheLocalAppFile);
@@ -96,38 +83,25 @@ public class Manager{
                 BufferedReader reader = new BufferedReader(new InputStreamReader(object.getObjectContent()));
                 queue.deleteMessage(QueueUrlLocalApps, currMessege);
 
-                System.out.println("Before second worker start:");
-
-                // Creatr Workers
+                // Create Workers
                 numberOfWorkersNeededForFile = numberOfLinesInTheLocalAppFile / 100;
                 workersForRest = (numberOfReceivedtasksFromTotalOfLocals.get() - numberOfTasks.get())/100;
                 allWorkersNeeded = numberOfWorkersNeededForFile+workersForRest;
-                numberOfActiveWorkers = ec2.getInstances("worker").size();
+                numberOfActiveWorkers = ec2.getInstances("").size() - ec2.getInstances("manager").size();
+
                 for (int i = 0; i< allWorkersNeeded-numberOfActiveWorkers; i++){
                     createworker(ec2);
                 }
-
-                System.out.println("Before close worker start:");
-
-                // Close Workers
-                /*while (allWorkersNeeded < numberOfActiveWorkers){
-                    ec2.terminateInstances(new ArrayList<>(ec2.getInstances("worker").subList(0,numberOfActiveWorkers-allWorkersNeeded)));
-                    --numberOfActiveWorkers;
-                }*/
-
 
                 // Create input file object
                 InputFileObject newFile = new InputFileObject(inputFileName, numberOfLinesInTheLocalAppFile, reader, inputFileID, summeryFilesIndicatorQueue);
                 InputFileObjectById.putIfAbsent(inputFileID, newFile);
 
                 // Create Completed tasks queue unique for the input file object
-                System.out.println("Before create completedTasks queue:");
-
                 queue.createQueue(inputFileID);
 
                 // calculate number of threads to open
                 int numberOfThreadsToLaunch = (numberOfLinesInTheLocalAppFile / 100) + 1;
-                System.out.println("\nNumber of threads to launch for the input file: " + inputFileName + " are: " + numberOfThreadsToLaunch + "\n");
 
                 // open input and output threads for a file from local app
                 for (int i = 0; i < numberOfThreadsToLaunch; ++i) {
@@ -135,26 +109,19 @@ public class Manager{
                     poolForOutput.execute(new OutputThread(newFile, numberOfCompletedTasks));
                 }
             }
-            System.out.println("Before upload loop:");
 
             boolean inputHasFinished = false;
             for (InputFileObject currFileObject :
                     InputFileObjectById.values()) {
                 if (currFileObject != null) {
                     synchronized (currFileObject) {
-                        System.out.println("Input fIle object before details: ");
-                        System.out.println(currFileObject);
                         currFileObject.setAllWorkersDone();
                         currFileObject.setRedAllLines();
-                        System.out.println("Input fIle object after details: ");
-
-                        System.out.println(currFileObject);
                         inputHasFinished = currFileObject.getAllWorkersDone();
                     }
                 }
                 if (inputHasFinished) {
                     synchronized (currFileObject) {
-
                         String outputName = currFileObject.getInputFilename() + "$";
                         String getSummeryFilesIndicatorQueue = currFileObject.getSummeryFilesIndicatorQueue();
                         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(path + outputName));
@@ -182,7 +149,6 @@ public class Manager{
 
 
     public static void createworker(EC2Object ec2){
-        System.out.println("in create worker");
         int managerinstance = ec2.getInstances("manager").size();
         if (managerinstance == 0){
             return;
