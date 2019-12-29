e1db7a3118c8515df722372cfe4864d94f81aa59
amirtal75 <46451642+amirtal75@users.noreply.github.com>
2019-12-29 02:51:09 +0200

    Update Queue.java

--- Project1/src/main/java/Queue.java
+++ Project1/src/main/java/Queue.java
@@ -75,7 +75,7 @@ public class Queue {
     }
 
     public void sendMessage(String queueUrl, String message) {
-
+        System.out.println("is sending a message to: " + queueUrl + message);
         try {
             SendMessageResult sendMessageResult = this.sqs.sendMessage(new SendMessageRequest(queueUrl, message));
         } catch (AmazonServiceException ase) {
@@ -93,10 +93,12 @@ public class Queue {
             List<Message> messages = this.sqs.receiveMessage(receiveMessageRequest).getMessages();
             return messages;
         } catch (AmazonServiceException ase) {
-            System.out.println("failed to receive message from the queue: " + queueUrl);
+            System.out.println(Thread.currentThread().getId() + "has failed to receive message");
+            printServiceError(ase);
 
         } catch (AmazonClientException ace) {
-            System.out.println("failed to receive message from the queue: " + queueUrl);
+            System.out.println(Thread.currentThread().getId()  + "has failed to receive message");
+            printClientError(ace);
         }
         return null;
     }
@@ -157,9 +159,20 @@ public class Queue {
         }
     }
 
+    public boolean queueExists(String queueName){
+        boolean queueExist = false;
+        for (String url:
+                new Queue().getQueueList()) {
+            if (url.contains(queueName)) {
+                queueExist = true;
+            }
+        }
+        return queueExist;
+    }
+
     private void printServiceError(AmazonServiceException ase){
         System.out.println("Caught an AmazonServiceException, which means your request made it " +
-                "to Amazon S3, but was rejected with an error response for some reason.");
+                "to Amazon SQS, but was rejected with an error response for some reason.");
         System.out.println("Error Message:    " + ase.getMessage());
         System.out.println("HTTP Status Code: " + ase.getStatusCode());
         System.out.println("AWS Error Code:   " + ase.getErrorCode());
@@ -168,7 +181,7 @@ public class Queue {
     }
     private void printClientError(AmazonClientException ace){
         System.out.println("Caught an AmazonClientException, which means the client encountered " +
-                "a serious internal problem while trying to communicate with S3, such as not " +
+                "a serious internal problem while trying to communicate with SQS, such as not " +
                 "being able to access the network.");
         System.out.println("Error Message: " + ace.getMessage());
     }
